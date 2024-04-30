f88ebb8baf6ac4eff45e9103ed98c5e5772874b0
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-04-30 11:49:33 +0530

    creating the project using spring initializer.

---
+++ src/main/java/com/rusiruchapana/springboot/CRUD/Rest/API/Application.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.springboot.CRUD.Rest.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
